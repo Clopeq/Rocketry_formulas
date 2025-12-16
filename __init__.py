13bb63973dbf9c3975c2faa1d4657a297161b5c0
Seba <259813@student.pwr.edu.pl>
2025-12-16 22:11:55 +0100

    add __init__

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# src/PyPep/__init__.py
+__version__ = "0.1.0"
+
+from .analysis import *
+from .rocketry import *
+
+# Control what "from PyPep import *" exposes
+# __all__ = [
+#     "PyPep",
+# ]
+
